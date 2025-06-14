48939eba8be77ff68effc425f0c6ac63cd142460
janarthanan <janajanart7@gmail.com>
2025-06-14 16:07:41 +0530

    first commit

---
+++ java script/pro.js
@@ -0,0 +1,11 @@
+var marks =[90,98,99,97,100];
+var [m1,m2,m3,m4,m5] =marks;
+console.log(m1);
+console.log(m2);
+console.log(m3);
+console.log(m4);
+console.log(m5);
+
+
+
+
---
+++ java script/script.js
@@ -0,0 +1,42 @@
+var a="10";
+var b="20";
+console.log(a+b)
+
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+ 
+var mark=78;
+var result=(mark>=80)? "o":(mark>=60? "a+":(mark>=35? "p":"f"));
+console.log(result);
+
+
+ 
+let arr =[10,20,30,40,];
+let arr2 =[...arr,50,60,70]
+console.log(arr);
+console.log(arr2);
+
+
+ 
+    
+
+
+
+
+
+
+
+console.log(typeof a);
+console.log(typeof b);
+a=true
+console.log(typeof a);
+var c;
+console.log(typeof c);
+var c=null;
+console.log(c);
+
+
+
+
